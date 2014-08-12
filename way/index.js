5f61b0eb0f588cab184afde58991e45cdf9507d5
Gwen <gwendall.esnault@gmail.com>
2014-08-12 20:27:44 +0200

    ok 2

--- way/index.js
+++ way/index.js
@@ -8,21 +8,4 @@ $(document).ready(function() {
 		console.log('"formData" property changed.', value);
 	});
 	
-	/*
-	way.watch("htmlData.value", function(value) {
-		_.delay(function() {
-			_.throttle(function() {
-				if (!way.get("inputChanged")) {
-					way.set("inputChanged", true);
-					$('.alert-change').fadeIn(300, function() {});
-					_.delay(function() {
-						way.set("inputChanged", false);
-						$('.alert-change').fadeOut(300, function() {});
-					}, 2000);		
-				}
-			}, 1000)();
-		}, 1000);
-	});
-	*/
-
 });
