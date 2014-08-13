a889235516fcc5a7a4b876f12a86972c4b635882
Gwen <gwendall.esnault@gmail.com>
2014-08-13 02:50:53 +0200

    ok 2

--- way/index.js
+++ way/index.js
@@ -3,9 +3,18 @@ $(document).ready(function() {
 	$(document).on("click", ".clearWay", function() {
 		way.remove();
 	});
+		
+	way.watchAll(function(selector, value) {
+		console.log("Something changed.", {
+			selector: selector,
+			value: value
+		});
+	});
 	
+	/*
 	way.watch("formData", function(value) {
 		console.log('"formData" property changed.', value);
 	});
+	*/
 	
 });
