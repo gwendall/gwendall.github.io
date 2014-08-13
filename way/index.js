1c55e16bda6f6298210879dc6c62764a0ca695d3
Gwen <gwendall.esnault@gmail.com>
2014-08-13 15:26:46 +0200

    ok 12

--- way/index.js
+++ way/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$(document).on("click", ".clearWay", function() {
 		way.clear();
 	});
-		
+
 	way.watchAll(function(selector, value) {
 		console.log("Something changed.", {
 			selector: selector,
