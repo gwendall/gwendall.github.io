91c674214eef22e583a4454879a2cb99e5dc52fc
Gwen <gwendall.esnault@gmail.com>
2014-08-15 15:13:24 +0200

    console back

--- way/index.js
+++ way/index.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
 	
-//	way.set("nationalities", ["french", "british", "chinese", "spanish"]);
-	
-	/*
 	way.watchAll(function(selector, value) {
 		console.log("Something changed.", {
 			selector: selector,
 			value: value
 		});
 	});
-	*/
 	
 });
